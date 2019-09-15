fa423e34fe588262f9c9a7094136cdbac6d7e73f
TedOnyango <teddonyango@gmail.com>
2019-09-15 21:50:57 +0300

    Connected the app to Firebase

---
+++ app/src/main/java/com/ted/barterapp/BarterItem.java
@@ -0,0 +1,69 @@
+package com.ted.barterapp;
+
+public class BarterItem {
+    private String id;
+    private String title;
+    private String description;
+    private String estimatedValue;
+    private String preferredItmes;
+    private String imageUrl;
+
+
+    public BarterItem(String title, String description, String estimatedValue, String preferredItmes, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.estimatedValue = estimatedValue;
+        this.preferredItmes = preferredItmes;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEstimatedValue() {
+        return estimatedValue;
+    }
+
+    public void setEstimatedValue(String estimatedValue) {
+        this.estimatedValue = estimatedValue;
+    }
+
+    public String getPreferredItmes() {
+        return preferredItmes;
+    }
+
+    public void setPreferredItmes(String preferredItmes) {
+        this.preferredItmes = preferredItmes;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
+
--- app/src/main/java/com/ted/barterapp/InsertActivity.java
+++ app/src/main/java/com/ted/barterapp/InsertActivity.java
@@ -1,33 +1,89 @@
 package com.ted.barterapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class InsertActivity extends AppCompatActivity {
-    ImageView itemImage;
-    EditText itemTitle;
-    EditText itemDescription;
-    EditText itemValue;
-    EditText preferredItems;
+    private FirebaseDatabase mFirebasedatabase;
+    private DatabaseReference mDatabaseReference;
+    EditText txtTitle;
+    EditText txtDescription;
+    EditText txtEstimatedValue;
+    EditText txtPreferredItmes;
+    ImageView txtImageUrl;
     Button saveBtn;
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+        mFirebasedatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebasedatabase.getReference().child("barterapp");
 
-        itemImage = (ImageView) findViewById(R.id.itemImage);
-        itemTitle = (EditText) findViewById(R.id.itemTitle);
-        itemDescription = (EditText) findViewById(R.id.itemDescription);
-        itemValue = (EditText) findViewById(R.id.itemValue);
-        preferredItems = (EditText) findViewById(R.id.preferredItems);
 
+        txtTitle = (EditText) findViewById(R.id.itemTitle);
+        txtDescription = (EditText) findViewById(R.id.itemDescription);
+        txtEstimatedValue = (EditText) findViewById(R.id.itemValue);
+        txtPreferredItmes = (EditText) findViewById(R.id.preferredItems);
+//        txtImageUrl = (ImageView) findViewById(R.id.itemImage);
         saveBtn = (Button) findViewById(R.id.saveBtn);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveBarterItem();
+                Toast.makeText(this,"Item saved", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void clean() {
+        txtTitle.setText("");
+        txtDescription.setText("");
+        txtEstimatedValue.setText("");
+        txtPreferredItmes.setText("");
+        txtTitle.requestFocus();
+    }
+
+    private void saveBarterItem() {
+        String title = txtTitle.getText().toString();
+        String description = txtDescription.getText().toString();
+        String estimateValue = txtEstimatedValue.getText().toString();
+        String preferredItems = txtPreferredItmes.getText().toString();
+        BarterItem item = new BarterItem(title, description, estimateValue, preferredItems, "");
+        mDatabaseReference.push().setValue(item);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
     }
 }
